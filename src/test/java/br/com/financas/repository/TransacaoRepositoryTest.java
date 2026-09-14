d4665e9378cdc337a80f3952fbac0e890aa543a6
Nicolas Lopes <fer02nick@gmail.com>
2026-09-14 17:38:01 -0300

    New packages and organization

---
+++ src/test/java/br/com/financas/repository/TransacaoRepositoryTest.java
@@ -0,0 +1,5 @@
+package br.com.financas.repository;
+
+public class TransacaoRepositoryTest {
+    
+}
--- src/test/java/br/com/financas/service/GerenciadorFinanceiroTest.java
+++ src/test/java/br/com/financas/service/GerenciadorFinanceiroTest.java
@@ -1,3 +1,4 @@
+package br.com.financas.service;
 class GerenciadorFinanceiroTest {
 
     @Test
